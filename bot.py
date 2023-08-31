1b2798093881a77c7a9eb82bac7d92321bf1bc84
Pavel <pas-zhukov@yandex.ru>
2023-08-31 20:18:22 +0300

    setup conversation handler

--- bot.py
+++ bot.py
@@ -36,16 +36,21 @@ def main():
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states={
-            State.START: ...
-        }
+            State.START: [
+                MessageHandler(
+                    Filters.regex('^Новый вопрос'), handle_new_question_request
+                ),
+            ],
+            State.ANSWERING: [
+                MessageHandler(
+                    Filters.text & ~Filters.command, handle_solution_attempt
+                ),
+            ],
+        },
+        fallbacks=[],
     )
 
-    start_handler = CommandHandler('start', start)
-    text_handler = MessageHandler(Filters.text & ~Filters.command, reply_to_msg)
-
-    dispatcher.add_handler(start_handler)
-    dispatcher.add_handler(text_handler)
-
+    dispatcher.add_handler(conv_handler)
     updater.start_polling()
     updater.idle()
 
@@ -55,25 +60,33 @@ def start(update: Update, _: CallbackContext):
                        ['Мой счёт']]
     reply_markup = ReplyKeyboardMarkup(custom_keyboard)
     update.message.reply_text("Здравствуйте!", reply_markup=reply_markup)
+    return State.START
 
 
-def reply_to_msg(update: Update, context: CallbackContext):
+def handle_new_question_request(update: Update, context: CallbackContext):
     redis_db = context.bot_data['redis_db']
     questions_db = context.bot_data['questions_db']
     user = update.message.from_user.id
-    if update.message.text == 'Новый вопрос':
-        with questions_db:
-            question = questions_db.get_random_question()
-        update.message.reply_text(question['question'])
-        redis_db.set(user, question['id'])
+    with questions_db:
+        question = questions_db.get_random_question()
+    update.message.reply_text(question['question'])
+    redis_db.set(user, question['id'])
+    return State.ANSWERING
+
+
+def handle_solution_attempt(update: Update, context: CallbackContext):
+    redis_db = context.bot_data['redis_db']
+    questions_db = context.bot_data['questions_db']
+    user = update.message.from_user.id
+    question_id = redis_db.get(user)
+    with questions_db:
+        ans = questions_db.get_question(question_id)['answer']
+    if ans.split('.')[0] in update.message.text:
+        update.message.reply_text('Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»')
+        return State.START
     else:
-        question_id = redis_db.get(user)
-        with questions_db:
-            ans = questions_db.get_question(question_id)['answer']
-        if ans.split('.')[0] in update.message.text:
-            update.message.reply_text('Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»')
-        else:
-            update.message.reply_text('Неправильно… Попробуешь ещё раз?')
+        update.message.reply_text('Неправильно… Попробуешь ещё раз?')
+        return State.ANSWERING
 
 
 if __name__ == '__main__':
